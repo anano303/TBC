eb37e217c869fb913ca4a3c9891e036e25c71dc3
anano303 <124179773+anano303@users.noreply.github.com>
2024-07-24 17:14:28 +0400

    footer mobile

--- script.js
+++ script.js
@@ -133,3 +133,18 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// const questions = document.querySelectorAll(".name");
+
+// questions.forEach((question) =>
+//   question.addEventListener("click", () => {
+//     if (question.parentNode.classList.contains("active")) {
+//       question.parentNode.classList.toggle("active");
+//     } else {
+//       questions.forEach((question) =>
+//         question.parentNode.classList.remove("active")
+//       );
+//       question.parentNode.classList.add("active");
+//     }
+//   })
+// );
